2e115525ddba68481dc35a3f7224763429b703e5
Knuutti <96994953+Knuutti@users.noreply.github.com>
2022-12-03 07:40:49 +0200

    Day 3 solutions

---
+++ Day 3/D3_1.py
@@ -0,0 +1,36 @@
+def main():
+
+    data_file = open("D3_data.txt", 'r')
+    data = data_file.readlines()
+    data_file.close()
+
+    sum_all = 0
+
+    for rucksack in data:
+
+        l = len(rucksack) // 2  # compartment length
+        
+        # Defining the compartments
+        v1 = rucksack[0:l]
+        v2 = rucksack[l:len(rucksack)]
+
+        for char in v1:
+
+            if char in v2:
+
+                # Calculating the priority value based on the ASCII value
+                if ord(char) > 90:
+                    sum_all+=(ord(char)-96)
+                else:
+                    sum_all+=(ord(char)-38)
+
+                break
+
+    print(f"Sum of the priorities: {sum_all}")
+
+
+
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ Day 3/D3_2.py
@@ -0,0 +1,37 @@
+def main():
+
+    data_file = open("D3_data.txt", 'r')
+
+    sum_all = 0
+
+    while True:
+
+        # Storing the three lines to separate variables
+        v1 = data_file.readline()
+
+        # No more data, breaking the loop
+        if len(v1) == 0:
+            break
+
+        # Reading the other two lines
+        v2 = data_file.readline()
+        v3 = data_file.readline()
+
+        # Finding the common character
+        for char in v1:
+
+            if char in v2 and char in v3:
+
+                if ord(char) > 90:
+                    sum_all+=(ord(char)-96)
+                else:
+                    sum_all+=(ord(char)-38)
+
+                break
+
+    print(f"Sum of the priorities: {sum_all}")
+
+    return
+
+if __name__ == "__main__":
+    main()
