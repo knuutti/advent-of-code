75cac283c6ee2491547f4425fe3b2ecba0811951
Knuutti <96994953+Knuutti@users.noreply.github.com>
2022-12-06 08:26:42 +0200

    Simplifed the solution by using dictionary

--- Day 6/D6.py
+++ Day 6/D6.py
@@ -1,47 +1,24 @@
 # Advent of Code 2022 - Day 6
 # Eetu Knutars / @knuutti
 
-def main():
-
-    N = 14  # the length of the desired queue (4 for part 1, 14 for part 2)
-
-    file_name = "D6_data.txt"
+def analyse(file_name, N):
 
     data_file = open(file_name, 'r')
     data = data_file.readline()
 
-    char_queue = [None] * (N-1)   # stores the previous N characters
-    dublicate = True        # stores the info whether there is a dublicate in the queue
+    for i in range(len(data)):
 
-    for i,char in enumerate(data):
+        # Removing the dublicate charactes in the past N characters
+        unique_chars = list( dict.fromkeys(data[i-N:i]) )
 
-        # Processed character is already in the queue, update the queue and go to the next one
-        if char in char_queue:
-            char_queue.pop(0)
-            char_queue.insert(N-2, char)
-            continue
-
-        # Loop for looking for dublicates in the existing queue
-        for j,old_char in enumerate(char_queue):
-            
-            # A dublicate is found -> update the value and break the loop
-            if old_char in char_queue[j+1:]:
-                dublicate = True
-                break
-
-            dublicate = False
-
-        # If there is a dublicate or the queue length is less than N, update the queue
-        if dublicate or not char_queue[0]:
-            char_queue.pop(0)
-            char_queue.insert(N-2, char)
-
-        # Processing has finished
-        else:
-            print(f"Number of processed characters: {i+1}")
+        # If all characters were unique, we found the last character
+        if len(unique_chars) == N:
+            print(f"Number of processed characters (N = {N}): {i}")
             break
 
 
 
 if __name__ == "__main__":
-    main()
+    file_name = "D6_data.txt"
+    analyse(file_name, 4)
+    analyse(file_name, 14)
