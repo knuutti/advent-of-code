c8b5db322d8db2ad77185c5ea4f9e1be30861b76
Knuutti <96994953+Knuutti@users.noreply.github.com>
2022-12-07 10:18:40 +0200

    Optimizations to the script

--- Day 6/D6.py
+++ Day 6/D6.py
@@ -1,24 +1,31 @@
 # Advent of Code 2022 - Day 6
 # Eetu Knutars / @knuutti
 
+import time
+
 def analyse(file_name, N):
 
     data_file = open(file_name, 'r')
     data = data_file.readline()
 
-    for i in range(len(data)):
+    for i in range(N, len(data)):
 
         # Removing the dublicate charactes in the past N characters
-        unique_chars = list( dict.fromkeys(data[i-N:i]) )
-
-        # If all characters were unique, we found the last character
-        if len(unique_chars) == N:
+        # If length of the dictionary is N, all characters are unique
+        if len(dict.fromkeys(data[i-N:i])) == N:
             print(f"Number of processed characters (N = {N}): {i}")
             break
 
 
 
 if __name__ == "__main__":
-    file_name = "D6_data.txt"
+    
+    start = time.time()
+
+    file_name = "bigboy.txt"
     analyse(file_name, 4)
     analyse(file_name, 14)
+
+    end = time.time()
+
+    print(f"Running time:\n{end-start:.1f}s")
