0463ba2922d56d4e0e06ffa22121ad5eb25027c9
Knuutti <96994953+Knuutti@users.noreply.github.com>
2022-12-13 10:51:42 +0200

    Part 1 solution

--- Day 13/D13.py
+++ Day 13/D13.py
@@ -3,8 +3,7 @@
 
 import json
 
-# STILL WORK IN PROGRESS
-# DOESN'T PRINT THE CORRECT ANSWER
+# only Part 1
 
 def main():
 
@@ -27,40 +26,47 @@ def main():
 
         order = compare(pair[0], pair[1])
         if order >= 0:
-            print("Order", 1+i*3)
             score += i+1
-        else:
-            print("Not Order", 1+ (i)*3)
 
-    print(score)
-            
+    print("Part 1:", score)
 
 def compare(left, right):
 
     order = 0
 
-    # Converting singular integers to lists
+    # If either one of the values is integer 
+    # while the other one is a list
     if type(left) is int and type(right) is not int:
             left = [left]
     elif type(right) is int and type(left) is not int:
             right = [right]
-    
-    #print("Compare", left, "vs", right)
 
+    # Both values are integers -> calculate the difference
     if type(left) is int:
         order = right - left
-    else: # lists
+    
+    # Both values are lists -> process them
+    else:
+
+        # If either one or both of the lists are empty
         if len(right) == 0 and len(left) > 0:
-            return -1
+            order = -1
         elif len(left) == 0 and len(right) > 0:
-            return 1
-        for i in range(len(left)):
-            order = compare(left[i], right[i])
-            if order != 0:
-                break
-            if len(right) == i + 1 and len(left) > i + 1:
-                order = -1
-                break
+            order = 1
+        elif len(left) == 0 and len(right) == 0:
+            order = 0
+        else:
+            for i in range(len(left)):
+                order = compare(left[i], right[i])
+                if order != 0:
+                    break
+                if len(right) == i + 1 and len(left) > i + 1:
+                    order = -1
+                    break
+                elif len(left) == i + 1 and len(right) > i + 1:
+                    order = 1
+                    break
+
 
     return order
 
