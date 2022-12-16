cea05a421ff911e7c1d7d0a70bd08cc87e35ce48
Knuutti <96994953+Knuutti@users.noreply.github.com>
2022-12-17 00:09:12 +0200

    Day 16 - Part 1

--- day_16/D16.py
+++ day_16/D16.py
@@ -1,3 +1,6 @@
+#   Advent of Code 2022 - Day 16
+#   Eetu Knutars / @knuutti
+
 class Graph():
  
     def __init__(self, vertices, graph):
@@ -16,23 +19,11 @@ class Graph():
                 min_index = v
  
         return min_index
-
-    def maxDistance(self, dist, sptSet):
- 
-        max = -10000000
- 
-        # Finding the nearest vertex which is not in the tree already
-        for v in range(self.V):
-            if dist[v] > max and sptSet[v] == False:
-                max = dist[v]
-                max_index = v
- 
-        return max_index
  
     # Function that implementing Dijkstra's algorithm
     def dijkstra(self, start):
  
-        dist = [-1000000] * self.V
+        dist = [1000000] * self.V
 
         # Distance to the start vertex is 0
         dist[start] = 0 
@@ -42,7 +33,7 @@ class Graph():
  
             # Pick the minimum distance vertex which is 
             # not in the shortest path tree
-            u = self.maxDistance(dist, sptSet)
+            u = self.minDistance(dist, sptSet)
  
             # Put the minimum distance vertex in 
             # the shortest path tree
@@ -53,7 +44,7 @@ class Graph():
             for v in range(self.V):
                 if (self.graph[u][v] > 0 and
                    sptSet[v] == False and
-                   dist[v] < dist[u] + self.graph[u][v]):
+                   dist[v] > dist[u] + self.graph[u][v]):
                     dist[v] = dist[u] + self.graph[u][v]
 
         # Returning a list containing the minimum distances of each vertex
@@ -61,7 +52,7 @@ class Graph():
 
 def main():
 
-    file = open('D16_example.txt', 'r')
+    file = open('D16_data.txt', 'r')
     data = file.read().splitlines()
     file.close()
 
@@ -70,9 +61,12 @@ def main():
     index = 0
     for line in data:
         temp = line.split('=')
-        valve_name = temp[0].rstrip(' has flow rate').lstrip('Valve ')
+        valve_name = temp[0].rstrip(' has flow rate')[6:]
         valve_names[valve_name] = index
         index += 1
+
+    valves = len(valve_names)
+    flow_values = [0] * valves
 
     # creating the matrix
     M = []
@@ -84,26 +78,66 @@ def main():
 
     for line in data:        
         temp = line.split('=')
-        index = valve_names[temp[0].rstrip(' has flow rate').lstrip('Valve ')]
+        index = valve_names[temp[0].rstrip(' has flow rate')[6:]]
         temp = temp[1].split(';')
-        flow = int(temp[0])
+        flow_values[index] = int(temp[0])
         temp = temp[1].split(', ')
 
-        M[index][valve_names[temp[0][len(temp[0])-2:len(temp[0])]]] = flow + 1
+        M[index][valve_names[temp[0][len(temp[0])-2:len(temp[0])]]] = 1
 
         for i in range(1, len(temp)):
-            M[index][valve_names[temp[i]]] = flow + 1
-
-
-    print(M)
+            M[index][valve_names[temp[i]]] = 1
 
     graph = Graph(len(valve_names), M)
 
-    dist = graph.dijkstra(0)  
-    print(dist)
+    
 
+    distances = []
+    for i in range(valves):
+        distances.append(graph.dijkstra(i))
 
+    ######################## ANALYSIS
 
+    total_flow, current_flow, current_location, best = 0, 0, valve_names['AA'], valve_names['AA']
+    time_remaining = 30
+
+    best_value = flow_values[best] * 2
+    for i in range(valves):
+        if i == current_location:
+            continue
+        # finding the best location to move first before opening a valve
+        flow_value = flow_values[i]/(distances[current_location][i])
+        if flow_value > best_value:
+            best = i
+            best_value = flow_value
+
+    time_remaining -= distances[current_location][best]   # moving to the valve
+    current_location = best # updating current location
+
+    while time_remaining > 0:
+        # opening the current valve
+        time_remaining -= 1
+        total_flow += time_remaining*flow_values[current_location]
+        flow_values[current_location] = 0 # open valve, no more extra flow available
+        
+        best_value = 0
+        for i in range(valves):
+            if i == current_location or distances[current_location][i] >= time_remaining:
+                continue
+            # finding the best location to move next
+            flow_value = flow_values[i]/(distances[current_location][i])
+            if flow_value > best_value:
+                best = i
+                best_value = flow_value
+
+        if best == current_location:
+            # no more optimal valves
+            break
+
+        time_remaining -= distances[current_location][best]
+        current_location = best
+        
+    print("Part 1:", total_flow)
 
 
 
