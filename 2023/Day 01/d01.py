99485647c7bc53f8adbd610ec2519e00ef97de3e
Knuutti <96994953+Knuutti@users.noreply.github.com>
2023-12-01 08:44:01 +0200

    day 1

--- 2023/Day 01/d01.py
+++ 2023/Day 01/d01.py
@@ -12,9 +12,10 @@ def main():
     print("Part 2:", sum_of_digits(data, 1))
 
 def find_digit(line, ran, mode):
+        
         str_nums = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
         for i in ran:
-            # Digit is numeric
+            # If an integer is found, return the value
             if line[i].isnumeric():
                 return int(line[i])
 
@@ -24,21 +25,27 @@ def find_digit(line, ran, mode):
 
             # Digit is spelled out
             for num in str_nums:
+                # Checked substring depends whether we are looking for the first
+                # digit or the last digit
                 if ran[0] == 0:
                     substr = line[:i+1]
                 else:
                     substr = line[i:]
+
+                # If spelled out digit is in the substring, return the value
                 if num in substr:
                     return str_nums[num]
 
 def sum_of_digits(data, mode):
-    nums = []
+
+    total = 0
     for line in data:
+        # Finding the first and last digit of each line
         first = find_digit(line, range(0, len(line)), mode)
         last = find_digit(line, range(len(line)-1, -1, -1), mode)
-        nums.append(10*first + last)
+        total += 10*first + last
 
-    return sum(nums)
+    return total
 
 if __name__ == "__main__":
     main()
