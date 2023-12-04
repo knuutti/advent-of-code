82fa5ffa83240b9cad453a8ac1d308c474b43c49
Knuutti <96994953+Knuutti@users.noreply.github.com>
2023-12-04 07:33:52 +0200

    Update d04.py

--- 2023/Day 04/d04.py
+++ 2023/Day 04/d04.py
@@ -4,37 +4,42 @@ def main():
     file = open(fname, 'r')
     data = file.read().splitlines()
 
-  
     total = 0
-    copies = []
-    for i in range(len(data)):
-        copies.append(1)
+    copies = [1 for _ in range(len(data))] 
 
     for i,line in enumerate(data):
-        wins = 0
-        winning = []
+
         line = line.split(": ")
         line = line[1].split(" | ")
-        for num in line[0].split(" "):
-            if num == "":
-                continue
-            winning.append(int(num))
-        for num in line[1].split(" "):
-            if num == "":
-                continue
-            if int(num) in winning:
-                wins += 1
+        winning_numbers = check_winning_numbers(line)
+        wins = check_wins(line, winning_numbers)
+
         if wins:
-            total += 2**(wins-1)
-            for j in range(i+1, i+wins+1):
+            total += 2**(wins-1) # update the points
+            for j in range(i+1, i+wins+1): # update the number of copies
                 if j < len(copies):
                     copies[j] += copies[i]*1
     
-    print(total)
-    print(sum(copies))
+    print("Part 1:", total)
+    print("Part 2:", sum(copies))
 
 
+def check_winning_numbers(line):
+    winning = []
+    for num in line[0].split(" "):
+        if num == "":
+            continue
+        winning.append(int(num))
+    return winning
 
+def check_wins(line, winning_numbers):
+    wins = 0
+    for num in line[1].split(" "):
+        if num == "":
+            continue
+        if int(num) in winning_numbers:
+            wins += 1
+    return wins
 
     
 if __name__ == "__main__":
