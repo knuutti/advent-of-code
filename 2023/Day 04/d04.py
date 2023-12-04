dc0e0700a46fcad052cd374c941ec92d83213932
Knuutti <96994953+Knuutti@users.noreply.github.com>
2023-12-04 08:23:37 +0200

    Cleaned up Day 4

--- 2023/Day 04/d04.py
+++ 2023/Day 04/d04.py
@@ -1,26 +1,21 @@
 def main():   
-    
     fname = "./2023/Day 04/input.txt"
     file = open(fname, 'r')
     data = file.read().splitlines()
-
-    total = 0
+    points = 0
     copies = [1 for _ in range(len(data))] 
 
     for i,line in enumerate(data):
-
-        line = line.split(": ")
-        line = line[1].split(" | ")
-        winning_numbers = check_winning_numbers(line[0])
-        wins = check_wins(line[1], winning_numbers)
-
+        line = line.split(":")[1].split("|")
+        winning_numbers = check_winning_numbers(line[0]) # get list of winning numbers
+        wins = check_wins(line[1], winning_numbers) # get number of wins
         if wins:
-            total += 2**(wins-1) # update the points
+            points += 2**(wins-1) # update the points
             for j in range(i+1, i+wins+1): # update the number of copies
                 if j < len(copies):
-                    copies[j] += copies[i]*1
+                    copies[j] += copies[i]*1 # multiply by copies of current card
     
-    print("Part 1:", total)
+    print("Part 1:", points)
     print("Part 2:", sum(copies))
 
 def check_winning_numbers(line):
