e7d11c7b3a4be5fe7fb53a0f263ebad76af9b3db
Knuutti <96994953+Knuutti@users.noreply.github.com>
2023-12-05 20:49:34 +0200

    Day 5 part 2

--- 2023/Day 05/d05.py
+++ 2023/Day 05/d05.py
@@ -1,121 +1,91 @@
 def main():   
-    fname = "./2023/Day 05/example.txt"
+    fname = "./2023/Day 05/input.txt"
     file = open(fname, 'r')
     data = file.read().splitlines()
 
-    min_location = 10000000
+    seed_info = [int(x) for x in data[0].lstrip("seeds: ").split()]
+    seed_ranges = []
+    for i in range(0, len(seed_info), 2):
+        seed_ranges.append([seed_info[i], seed_info[i]-1+seed_info[i+1]])
 
-    seeds = get_seeds(data) # gets range of seeds
-    print(seeds)
-    seeds = get_conversion(data.index("seed-to-soil map:"), data, seeds)
-    print(seeds)
-    
-    seeds = get_conversion(data.index("soil-to-fertilizer map:"), data, seeds)
-    print(seeds)
-    seeds = get_conversion(data.index("fertilizer-to-water map:"), data, seeds)
-    print(seeds)
-    seeds = get_conversion(data.index("water-to-light map:"), data, seeds)
-    print(seeds)
-    seeds = get_conversion(data.index("light-to-temperature map:"), data, seeds)
-    print(seeds)
-    seeds = get_conversion(data.index("temperature-to-humidity map:"), data, seeds)
-    print(seeds)
-    seeds = get_conversion(data.index("humidity-to-location map:"), data, seeds)
-    print(seeds)
-
-def get_seeds(data):
-    line = data[0].split()
-    line.pop(0)
-    
-    nums = [int(x) for x in line]
-    seeds = []
-    for i in range(0,len(nums)//2+1,2):
-        seeds.append([nums[i],nums[i]+nums[i+1]])
-
-    return seeds
-
-def get_conversion(index, data, seeds):
-    conversions = []
-    while index < len(data)-1:
-        index += 1
-        if len(data[index]) == 0:
-            break
-        [dest, src, ran] = [int(x) for x in data[index].split()]
-
-        conversions.append([src, src+ran, dest-src])
-
-    #print(conversions)
-    converted = []
-
-    
-
-    for seed in seeds:
+    conversion_ranges = []
+    topic = False
+    for line in data[3:]:
+        if topic: # header row
+            topic = False
+            continue
+        if len(line) == 0: # empty row
+            seed_ranges = new_ranges(seed_ranges, conversion_ranges)
+            conversion_ranges = []
+            topic = True
+            continue
         
+        line = [int(x) for x in line.split()]
+        conversion_ranges.append([line[1], line[1]+line[2]-1, line[0]-line[1]]) # start, end, delta
+    
+    seed_ranges = new_ranges(seed_ranges, conversion_ranges)
+    value = seed_ranges[0][0]
+    for seed in seed_ranges:
+        if seed[0] < value:
+            value = seed[0]
 
-        # conv0 = min source
-        # conv1 = max source
-        # conv2 = conversion term (src -> dest)
-        #print("----------")
-        #print(seed)
-        #print(conversions)
-
-        # given the range of seeds
-        # get range of outcomes
-
-        for conv in conversions:
-
-            min_converted = 10000000
-            max_converted = -1
-
-            min_local = seed[0]
-            max_local = seed[1]
-            # seed range
-            min_seed = seed[0]
-            max_seed = seed[1]
-
-            #print(min_seed, max_seed, conv)
-
-            # left seed range OUTSIDE conversion range
-            if min_seed <= conv[0] and max_seed >= conv[0]:
-                if conv[0]+conv[2] < min_converted:
-                    min_converted = conv[0]+conv[2]
-            # left seed range INSIDE conversion range
-            elif min_seed >= conv[0] and min_seed <= conv[1]:
-                min_local = min_seed + conv[2]
-                if min_seed + conv[2] < min_converted:
-                    min_converted = min_seed + conv[2]
-            
-            # right seed range INSIDE conversion range
-            if max_seed <= conv[1] and max_seed >= conv[0]:
-                max_local = max_seed + conv[2]
-                if max_seed + conv[2] > max_converted:
-                    max_converted = max_seed + conv[2]
-            # right seed range OUTSIDE conversion range
-            elif max_seed > conv[1] and min_seed <= conv[1]:
-                if conv[1]+conv[2] > max_converted:
-                    max_converted = conv[1]+conv[2]
-
-        
-            if min_converted == 10000000:
-                min_converted = seed[0]
-            if max_converted == -1:
-                max_converted = seed[1]
-
-            #print(min_converted, max_converted)
-
-            #print(min_converted, max_converted)
+    print("Part 2:", value)
 
 
 
-            if [min_converted, max_converted] not in converted:
-                converted.append([min_converted, max_converted])
 
-            # if [min_local, max_local] not in converted:
-            #     converted.append([min_local, max_local])
+def new_ranges(seed_ranges: list, conversion_ranges: list):
+        new_seed_ranges = []
+        while len(seed_ranges) > 0: # using while since we'll add stuff to seed_ranges still
+            seed = seed_ranges.pop(0)
+            x = len(new_seed_ranges)
+            for conversion in conversion_ranges:
 
+                if seed[0] == conversion[0]:
+                    new_seed_ranges.append([seed[0]+conversion[2],seed[0]+conversion[2]])
+                    seed_ranges.append([seed[0]+1,seed[1]])
+                    break
+                elif seed[0] == conversion[1]:
+                    new_seed_ranges.append([seed[0]+conversion[2],seed[0]+conversion[2]])
+                    break
+                elif seed[1] == conversion[0]:
+                    new_seed_ranges.append([seed[1]+conversion[2],seed[1]+conversion[2]])
+                    break
+                elif seed[1] == conversion[1]:
+                    new_seed_ranges.append([seed[1]+conversion[2],seed[1]+conversion[2]])
+                    seed_ranges.append([seed[0],seed[1]-1])
+                    break
 
+                if conversion[0]>seed[0] and conversion[1]<seed[1]: # conversion range inside seed range
+                    #print("1")
+                    new_seed_ranges.append([conversion[0]+conversion[2],conversion[1]+conversion[2]])
+                    seed_ranges.append([seed[0],conversion[0]-1])
+                    seed_ranges.append([conversion[1]+1, seed[1]])
+                    break
 
-    return converted
+                elif conversion[0]<seed[0] and conversion[1]>seed[1]: # seed range inside conversion range
+                    #print("2")                      
+                    new_seed_ranges.append([seed[0]+conversion[2],seed[1]+conversion[2]])
+                    break
+
+                elif conversion[0]>seed[0] and conversion[0]<seed[1] and conversion[1]>seed[1]: # open right side
+                    #print("3")
+                    new_seed_ranges.append([conversion[0]+conversion[2],seed[1]+conversion[2]])
+                    seed_ranges.append([seed[0],conversion[0]-1])
+                    break
+
+                elif conversion[0]<seed[0] and conversion[1]>seed[0] and conversion[1]<seed[1]: # open left side
+                    #print("4")
+                    new_seed_ranges.append([seed[0]+conversion[2],conversion[1]+conversion[2]])
+                    seed_ranges.append([conversion[1]+1,seed[1]])
+                    break
+
+            if x == len(new_seed_ranges):
+                new_seed_ranges.append(seed)
+
+                  
+        return new_seed_ranges
+                  
 
 if __name__ == "__main__":
     
