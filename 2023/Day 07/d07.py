93bbd29b0e69ad4e29c72f65b9947e36301b7dc1
Knuutti <96994953+Knuutti@users.noreply.github.com>
2023-12-07 18:25:10 +0200

    Optimizations for bigboy

--- 2023/Day 07/d07.py
+++ 2023/Day 07/d07.py
@@ -1,49 +1,34 @@
 # Advent of Code 2023 - Day 7
 # Eetu Knutars / @knuutti
 
+from collections import OrderedDict
+
 def main():   
-    fname = "./2023/Day 07/input.txt"
+    fname = "./2023/Day 07/day7_bigboy.txt"
     file = open(fname, 'r')
     data = file.read().splitlines()
     print("Part 1:",analyse(data,0))
     print("Part 2:",analyse(data,1))
 
+    # Doesn't work for data with dublicate hands
+
 def analyse(data, mode):
     # Generates hands using the Hand class
-    hands = []
+    hands = {}
+
     for i,row in enumerate(data):
         row = row.split()
-        hands.append(Hand(row[0],row[1], mode))
-    # Sorting the hands
-    ranked_hands = sort_hands(hands)
-    # Calculating the result (rank * bid)
-    total = 0
-    for i,h in enumerate(ranked_hands):
-        rank = (len(ranked_hands)-i)
-        total += rank*h.bid
-    return total
+        new_hand = Hand(row[0],row[1], mode)
+        hands[new_hand.level] = new_hand.bid
 
-# Sorting the hands based on their level and contents
-def sort_hands(hands):
-    ranked_hands = [hands[0]]
-    for hand in hands[1:]:
-        added = False
-        for i,hand_to_compare in enumerate(ranked_hands):
-            if hand.level > hand_to_compare.level:
-                ranked_hands.insert(i, hand)
-                added = True
-            elif hand.level == hand_to_compare.level:
-                for j in range(0,5):
-                    if hand.cards[j] > hand_to_compare.cards[j]:
-                        ranked_hands.insert(i, hand)
-                        added = True
-                        break
-                    elif hand.cards[j] < hand_to_compare.cards[j]:
-                        break
-            if added == True:
-                break
-        if added == False: ranked_hands.append(hand)
-    return ranked_hands
+    od = OrderedDict(sorted(hands.items(), reverse=True))
+
+    total = 0
+    for i,h in enumerate(od):
+        rank = (len(od)-i)
+        bid = od[h]
+        total += rank*bid
+    return total
 
 class Hand():
     def __init__(self, hand, bid, mode):
@@ -63,26 +48,28 @@ class Hand():
             for i in self.joker_indexes:
                 self.cards.insert(i,sorted_uniques[-1][0])
                 self.uniques[sorted_uniques[-1][0]] += 1
-                real_cards.insert(i,-1)
+                real_cards.insert(i,1)
             
             # Determining the level of the hand
-            # Level 4 = "5 uniques", ..., Level 0 = "high card"
-            level = 5 - len(list(self.uniques.values()))
+            level = 2*(5 - len(list(self.uniques.values())))
             
             # 4 uniques vs. full house
-            if level == 3 and max(list(self.uniques.values())) == 4:
-                    level += 0.5
+            if level == 6 and max(list(self.uniques.values())) == 4:
+                    level += 1
             # 3 uniques vs. 2 pairs
-            elif level == 2 and max(list(self.uniques.values())) == 3:
-                    level += 0.5
+            elif level == 4 and max(list(self.uniques.values())) == 3:
+                    level += 1
 
-            # Replacing the joker cards with value -1
+            # Replacing the joker cards with value 1
             self.cards = real_cards
 
-        else: # Special case where all cards are jokers
-            self.cards = [-1,-1,-1,-1,-1]
-            level = 4
+            level = (level)*(14**5)
+            real_cards.reverse()
+            for i,card in enumerate(real_cards):
+                level += (card-1)*(14**i)
 
+        else: # Special case where all cards are jokers
+            level = 8*(14**5)
         return level
 
     # Turns the string list of cards into integer list
@@ -91,7 +78,7 @@ class Hand():
          joker_indexes = []
          for i,card in enumerate(hand):
             value = self.get_card_value(card, mode)
-            if value != -1: 
+            if value != 1: 
                 self.cards.append(value)
             else: 
                 joker_indexes.append(i)
@@ -109,7 +96,7 @@ class Hand():
         if str_card == "K": return 13
         if str_card == "Q": return 12
         if str_card == "J": 
-            if mode: return -1
+            if mode: return 1
             else: return 11
         if str_card == "T": return 10
         return int(str_card)
