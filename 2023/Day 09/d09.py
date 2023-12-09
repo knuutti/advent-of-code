8a28e47b00b2b2b4c557b4282eec2e2e67ff14c9
Knuutti <96994953+Knuutti@users.noreply.github.com>
2023-12-09 08:52:10 +0200

    Cleaning

--- 2023/Day 09/d09.py
+++ 2023/Day 09/d09.py
@@ -7,25 +7,20 @@ def main():
     fname = "./2023/Day 09/input.txt"
     file = open(fname, 'r')
     data = file.read().splitlines()
-    silver = 0
-    gold = 0
+    silver, gold = 0,0
     for row in data:
-        row = [int(x) for x in row.split()]
-        next = predict(row, 0)
-        prev = predict(row, 1)
-        silver += next
-        gold += prev
+        nums = [int(x) for x in row.split()]
+        silver += predict(nums, 0)
+        gold += predict(nums, 1)
 
     print("Part 1:", silver)
     print("Part 2:", gold)
 
 # Using recursion to find the next or the previous number
 def predict(line, mode):
-    if sum(line) == 0:
-        return 0
-    else:
-        diffs = [line[i+1-mode]-line[i+mode] for i in range(0,len(line)-1)]
-        return line[-1+mode] + predict(diffs, mode)
+    if sum(line) == 0: return 0
+    diffs = [line[i+1-mode]-line[i+mode] for i in range(0,len(line)-1)]
+    return line[-1+mode] + predict(diffs, mode)
 
 
 if __name__ == "__main__":
