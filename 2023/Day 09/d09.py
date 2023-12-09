49f49df2f09fb823b935e3597bb91757e022b8a7
Knuutti <96994953+Knuutti@users.noreply.github.com>
2023-12-09 08:22:08 +0200

    Added comments

--- 2023/Day 09/d09.py
+++ 2023/Day 09/d09.py
@@ -1,3 +1,8 @@
+# Advent of Code 2023 - Day 8
+# Eetu Knutars / @knuutti
+
+# I love recursion when it's simple
+
 def main():   
     fname = "./2023/Day 09/input.txt"
     file = open(fname, 'r')
@@ -6,21 +11,21 @@ def main():
     gold = 0
     for row in data:
         row = [int(x) for x in row.split()]
-        next = recursive_thing(row, 0)
-        prev = recursive_thing(row, 1)
+        next = predict(row, 0)
+        prev = predict(row, 1)
         silver += next
         gold += prev
 
     print("Part 1:", silver)
     print("Part 2:", gold)
 
-
-def recursive_thing(line, mode):
+# Using recursion to find the next or the previous number
+def predict(line, mode):
     if sum(line) == 0:
         return 0
     else:
         diffs = [line[i+1-mode]-line[i+mode] for i in range(0,len(line)-1)]
-        return line[-1+mode] + recursive_thing(diffs, mode)
+        return line[-1+mode] + predict(diffs, mode)
 
 
 if __name__ == "__main__":
